3ac0ffb2ff4ca1afc799d683e73e9b3e5bb6f94b
Nekmo <contacto@nekmo.com>
2019-01-24 00:35:22 +0100

    action-panel component.

---
+++ ui/src/app/action/action-panel/action-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActionPanelComponent } from './action-panel.component';
+
+describe('ActionPanelComponent', () => {
+  let component: ActionPanelComponent;
+  let fixture: ComponentFixture<ActionPanelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActionPanelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActionPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
